0d51306e7ee27035b59f72acc84844bf57725090
laurentdupin <48155177+laurentdupin@users.noreply.github.com>
2026-07-24 12:12:34 +0200

    Exercise Metal multi-output lifecycle

--- modules/html_css/tests/hcsr_multi_output_smoke.gd
+++ modules/html_css/tests/hcsr_multi_output_smoke.gd
@@ -11,6 +11,9 @@ func _initialize() -> void:
 	elif "--vulkan" in OS.get_cmdline_user_args():
 		backend = HTMLView.BACKEND_VULKAN
 		backend_name = "Vulkan"
+	elif "--metal" in OS.get_cmdline_user_args():
+		backend = HTMLView.BACKEND_METAL
+		backend_name = "Metal"
 	call_deferred("_run")
 
 func _run() -> void:
