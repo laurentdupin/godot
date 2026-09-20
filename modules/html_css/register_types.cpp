974a8008aa0a44830e11a74fa34a45d6b8ce7e64
laurentdupin <48155177+laurentdupin@users.noreply.github.com>
2026-09-20 12:12:40 +0200

    Remove application desktop overlay from engine

--- modules/html_css/register_types.cpp
+++ modules/html_css/register_types.cpp
@@ -31,7 +31,6 @@
 #include "register_types.h"
 
 #include "html_document.h"
-#include "html_desktop_overlay.h"
 #include "html_render_target.h"
 #include "html_texture.h"
 #include "html_view.h"
@@ -69,7 +68,6 @@ void initialize_html_css_module(ModuleInitializationLevel p_level) {
 	}
 
 	GDREGISTER_CLASS(HTMLDocument);
-	GDREGISTER_CLASS(HTMLDesktopOverlay);
 	GDREGISTER_CLASS(HTMLTexture2D);
 	GDREGISTER_CLASS(HTMLRenderTarget);
 	GDREGISTER_CLASS(HTMLView);
